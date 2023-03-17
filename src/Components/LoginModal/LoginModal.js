9e55c82e334f6b35e6ed819cf248ead167f29151
PayHaz notebook <dragonx891253@gmail.com>
2023-03-18 02:24:38 +0500

    Login Modal Design

--- src/Components/LoginModal/LoginModal.js
+++ src/Components/LoginModal/LoginModal.js
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
-import { Modal, Form, Input, Button } from 'antd'
+import { Modal, Form, Input, Button, Checkbox, Typography } from 'antd'
+import './LoginModal.css'
+import styles from './LoginModal.less'
+
+const { Text } = Typography
 
 const LoginModal = ({ visible, handleOk, handleCancel }) => {
 	const [form] = Form.useForm()
 	const [loading, setLoading] = useState(false)
+	const [rememberPassword, setRememberPassword] = useState(false)
 
 	const onFinish = (values) => {
 		setLoading(true)
@@ -16,15 +22,51 @@ const LoginModal = ({ visible, handleOk, handleCancel }) => {
 	return (
 		<Modal
 			visible={visible}
-			title='Войти в систему'
+			title='Вход'
+			className={styles.booking_information_table}
 			onCancel={handleCancel}
 			footer={[
-				<Button key='back' onClick={handleCancel}>
-					Отмена
-				</Button>,
-				<Button key='submit' type='primary' loading={loading} onClick={() => form.submit()}>
-					Войти
-				</Button>,
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						marginTop: 10,
+					}}
+				>
+					<div>
+						<Checkbox checked={rememberPassword} onChange={(e) => setRememberPassword(e.target.checked)}>
+							Запомнить пароль
+						</Checkbox>
+					</div>
+					<div>
+						<a href='#'>Забыли пароль?</a>
+					</div>
+				</div>,
+				<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 10 }}>
+					<Button key='submit' type='primary' loading={loading} onClick={() => form.submit()}>
+						Войти
+					</Button>
+				</div>,
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center',
+						marginTop: 10,
+						flexDirection: 'column',
+						paddingTop: '15px',
+					}}
+				>
+					<div>
+						<Text>Нет аккаунта?</Text>
+					</div>
+					<div>
+						<Button type='link' onClick={handleCancel} style={{ marginLeft: 5 }}>
+							Зарегистрироваться
+						</Button>
+					</div>
+				</div>,
 			]}
 		>
 			<Form form={form} onFinish={onFinish}>
