39655cda1835912fc0ffb5f863efc95cf76fd97c
Vadim Ermakov <vadim.ermakov-89@mail.ru>
2023-04-02 02:30:48 +0500

    Added second step content

--- src/Components/AddItemPage/AddItemPage.js
+++ src/Components/AddItemPage/AddItemPage.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Divider, Steps, Cascader, Button, message } from 'antd'
+import './AddItemPage.css'
 
 const options = [
 	{
@@ -61,44 +62,127 @@ const steps = [
 
 const AddItemPage = () => {
 	const [current, setCurrent] = useState(0)
-	const [category, setСategory] = useState()
+	const [category, setCategory] = useState()
+	const [items, setItems] = useState([])
+	const [nextStepButton, setNextStepButton] = useState(true)
+
+	const [formData, setFormData] = useState({
+		category: null,
+		name: '',
+		characteristics: '',
+		description: '',
+	})
 
 	const onChange = (value) => {
 		setCurrent(value)
-	}
-
-	const onCategoryChange = (value) => {
-		setСategory(value)
+		console.log(value)
 	}
 
 	const next = () => {
 		setCurrent(current + 1)
+		setNextStepButton(true)
+	}
+
+	useEffect(() => {
+		setItems(
+			steps.map((item) => ({
+				key: item.title,
+				title: item.title,
+				description: item.description,
+				disabled: item.disabled,
+			}))
+		)
+	}, [])
+
+	const onCategoryChange = (value) => {
+		setCategory(value)
+		if (current === 0 && value) {
+			setNextStepButton(false)
+			setFormData({ ...formData, category: value })
+			setItems((prevItems) => {
+				const newItems = [...prevItems]
+				newItems[1].disabled = false
+				return newItems
+			})
+		}
+		if (value === undefined) {
+			setNextStepButton(true)
+			setItems((prevItems) => {
+				const newItems = [...prevItems]
+				newItems[1].disabled = true
+				return newItems
+			})
+		}
 	}
 
 	const StepContent = () => {
+		const { category, name, characteristics, description } = formData
+		const onInputChange = (e) => {
+			setFormData({ ...formData, [e.target.name]: e.target.value })
+			if (
+				current === 1 &&
+				formData.name !== '' &&
+				formData.characteristics !== '' &&
+				formData.description !== ''
+			) {
+				setNextStepButton(false)
+			} else {
+				setNextStepButton(true)
+			}
+		}
 		if (current === 0)
 			return (
-				<Cascader options={options} value={category} onChange={onCategoryChange} placeholder='Please select' />
+				<Cascader
+					options={options}
+					value={category}
+					onChange={onCategoryChange}
+					placeholder='Выберите категорию'
+					className='cascader'
+				/>
+			)
+		if (current === 1)
+			return (
+				<div>
+					<label>Название:</label>
+					<input
+						type='text'
+						name='name'
+						value={name}
+						onChange={onInputChange}
+						placeholder='Введите название'
+						className='form-control'
+					/>
+					<label>Характеристики:</label>
+					<input
+						type='text'
+						name='characteristics'
+						value={characteristics}
+						onChange={onInputChange}
+						placeholder='Введите характеристики'
+						className='form-control'
+					/>
+					<label>Описание:</label>
+					<textarea
+						name='description'
+						value={description}
+						onChange={onInputChange}
+						placeholder='Введите описание'
+						className='form-control'
+					/>
+				</div>
 			)
 	}
-
-	const items = steps.map((item) => ({
-		key: item.title,
-		title: item.title,
-		description: item.description,
-		disabled: item.disabled,
-	}))
 
 	return (
 		<div className='container'>
 			<div className='row'>
-				<h1>Категория</h1>
+				<h1>Новое объявление</h1>
 				<Steps current={current} onChange={onChange} items={items} />
-				<div className='pt-5'>
+				<div className='pt-5 change__category__group'>
 					{StepContent()}
 					{current < steps.length - 1 && (
-						<Button type='primary' onClick={() => next()}>
-							Next
+						<Button type='primary' className='mt-3' disabled={nextStepButton} onClick={() => next()}>
+							Следующий шаг
 						</Button>
 					)}
 					{current === steps.length - 1 && (
