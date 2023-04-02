ce53897cf2bdf940413b7ffc8fa48b2a55a9e632
Vadim Ermakov <vadim.ermakov-89@mail.ru>
2023-04-02 22:23:52 +0500

    Added dynamic characteristics

--- src/Components/AddItemPage/AddItemPage.js
+++ src/Components/AddItemPage/AddItemPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import { Divider, Steps, Cascader, Button, message } from 'antd'
+import { Divider, Steps, Cascader, Button, message, Input, Form, Space } from 'antd'
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons'
 import './AddItemPage.css'
 
 const options = [
@@ -66,10 +67,14 @@ const AddItemPage = () => {
 	const [items, setItems] = useState([])
 	const [nextStepButton, setNextStepButton] = useState(true)
 
+	const [formCategory] = Form.useForm()
+	const [formDatas] = Form.useForm()
+	const [, forceUpdate] = useState({})
+
 	const [formData, setFormData] = useState({
 		category: null,
 		name: '',
-		characteristics: '',
+		characteristics: [],
 		description: '',
 	})
 
@@ -81,6 +86,12 @@ const AddItemPage = () => {
 	const next = () => {
 		setCurrent(current + 1)
 		setNextStepButton(true)
+		console.log(formData)
+	}
+
+	const onFinish = (values) => {
+		next()
+		setNextStepButton(false)
 	}
 
 	useEffect(() => {
@@ -92,6 +103,7 @@ const AddItemPage = () => {
 				disabled: item.disabled,
 			}))
 		)
+		forceUpdate({})
 	}, [])
 
 	const onCategoryChange = (value) => {
@@ -126,49 +138,165 @@ const AddItemPage = () => {
 				formData.description !== ''
 			) {
 				setNextStepButton(false)
+				setItems((prevItems) => {
+					const newItems = [...prevItems]
+					newItems[2].disabled = false
+					return newItems
+				})
 			} else {
 				setNextStepButton(true)
+				setItems((prevItems) => {
+					const newItems = [...prevItems]
+					newItems[2].disabled = true
+					return newItems
+				})
 			}
 		}
 		if (current === 0)
 			return (
-				<Cascader
-					options={options}
-					value={category}
-					onChange={onCategoryChange}
-					placeholder='Выберите категорию'
-					className='cascader'
-				/>
+				<Form
+					name='dynamic_form_nest_item'
+					form={formCategory}
+					onFinish={onFinish}
+					style={{
+						maxWidth: 600,
+					}}
+					autoComplete='off'
+				>
+					<Form.Item
+						name='category'
+						label='Категория'
+						rules={[{ required: true, message: 'Пожалуйста, выберите категорию' }]}
+					>
+						<Cascader
+							options={options}
+							value={category}
+							onChange={onCategoryChange}
+							placeholder='Выберите категорию'
+							className='cascader'
+						/>
+					</Form.Item>
+					<Form.Item shouldUpdate>
+						{() => (
+							<Button
+								type='primary'
+								htmlType='submit'
+								disabled={
+									!formCategory.isFieldsTouched(true) ||
+									!!formCategory.getFieldsError().filter(({ errors }) => errors.length).length
+								}
+							>
+								Следующий шаг
+							</Button>
+						)}
+					</Form.Item>
+				</Form>
 			)
 		if (current === 1)
 			return (
 				<div>
-					<label>Название:</label>
-					<input
-						type='text'
-						name='name'
-						value={name}
-						onChange={onInputChange}
-						placeholder='Введите название'
-						className='form-control'
-					/>
-					<label>Характеристики:</label>
-					<input
-						type='text'
-						name='characteristics'
-						value={characteristics}
-						onChange={onInputChange}
-						placeholder='Введите характеристики'
-						className='form-control'
-					/>
-					<label>Описание:</label>
-					<textarea
-						name='description'
-						value={description}
-						onChange={onInputChange}
-						placeholder='Введите описание'
-						className='form-control'
-					/>
+					<Form
+						name='dynamic_form_nest_item'
+						onFinish={onFinish}
+						style={{
+							maxWidth: 600,
+						}}
+						form={formDatas}
+						autoComplete='off'
+					>
+						<Form.Item
+							name='name'
+							label='Название:'
+							rules={[{ required: true, message: 'Пожалуйста, введите название' }]}
+						>
+							<Input
+								type='text'
+								name='name'
+								value={name}
+								onChange={onInputChange}
+								placeholder='Введите название'
+								className='form-control'
+							/>
+						</Form.Item>
+
+						<label>Характеристики:</label>
+
+						<Form.List name='users'>
+							{(fields, { add, remove }) => (
+								<>
+									{fields.map(({ key, name, ...restField }) => (
+										<Space
+											key={key}
+											style={{
+												display: 'flex',
+												marginBottom: 8,
+											}}
+											align='baseline'
+										>
+											<Form.Item
+												{...restField}
+												name={[name, 'first']}
+												rules={[
+													{
+														required: true,
+														message: 'Missing first name',
+													},
+												]}
+											>
+												<Input placeholder='Введите параметр' />
+											</Form.Item>
+											<Form.Item
+												{...restField}
+												name={[name, 'last']}
+												rules={[
+													{
+														required: true,
+														message: 'Missing last name',
+													},
+												]}
+											>
+												<Input placeholder='Введите характеристику' />
+											</Form.Item>
+											<MinusCircleOutlined onClick={() => remove(name)} />
+										</Space>
+									))}
+									<Form.Item>
+										<Button type='dashed' onClick={() => add()} block icon={<PlusOutlined />}>
+											Add field
+										</Button>
+									</Form.Item>
+								</>
+							)}
+						</Form.List>
+						<Form.Item
+							name='description'
+							label='Описание:'
+							rules={[{ required: true, message: 'Пожалуйста, введите описание' }]}
+						>
+							<Input
+								name='description'
+								value={description}
+								onChange={onInputChange}
+								placeholder='Введите описание'
+								className='form-control'
+							/>
+						</Form.Item>
+
+						<Form.Item shouldUpdate>
+							{() => (
+								<Button
+									type='primary'
+									htmlType='submit'
+									disabled={
+										!formDatas.isFieldsTouched(true) ||
+										!!formDatas.getFieldsError().filter(({ errors }) => errors.length).length
+									}
+								>
+									Следующий шаг
+								</Button>
+							)}
+						</Form.Item>
+					</Form>
 				</div>
 			)
 	}
@@ -180,11 +308,7 @@ const AddItemPage = () => {
 				<Steps current={current} onChange={onChange} items={items} />
 				<div className='pt-5 change__category__group'>
 					{StepContent()}
-					{current < steps.length - 1 && (
-						<Button type='primary' className='mt-3' disabled={nextStepButton} onClick={() => next()}>
-							Следующий шаг
-						</Button>
-					)}
+
 					{current === steps.length - 1 && (
 						<Button type='primary' onClick={() => message.success('Processing complete!')}>
 							Done
